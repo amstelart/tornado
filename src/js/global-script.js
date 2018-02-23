3c644dd959ddc0d94e507678bd1634a330f01d42
Артем <amstelart@MacBook-Pro-Artem.local>
2018-02-23 16:53:20 +0300

    all pages

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,81 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+  $(".main-slider").owlCarousel({
+    items: 1,
+    nav: false,
+    dots: true,
+    loop: true,
+    responsive : {
+      1300 : {
+        nav: true
+      }
+    }
+  });
+  $(".image-gallery").owlCarousel({
+    items: 5,
+    nav: false,
+    loop: false,
+    margin: 30,
+    center: false,
+    responsive : {
+      0 : {
+        items: 2,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 2,
+        loop: true
+      },
+      768 : {
+        items: 4,
+        loop: true
+      },
+      992 : {
+        items: 4,
+        loop: true
+      },
+      1200 : {
+        items: 5,
+      },
+      1800 : {
+        items: 5,
+      }
+    }
+  });
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    nav: false,
+    loop: false,
+    margin: 30,
+    center: false,
+    responsive : {
+      0 : {
+        items: 1,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 1,
+        loop: true
+      },
+      768 : {
+        items: 2,
+        loop: true
+      },
+      992 : {
+        items: 2,
+        loop: true
+      },
+      1200 : {
+        items: 3,
+      },
+      1800 : {
+        items: 3,
+      }
+    }
+  });
+});
 
 // Изоляция без jQuery
 // (function(){
