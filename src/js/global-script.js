01e75afbd719158658eb99e7654fdaf96be9e111
Артем <amstelart@MacBook-Pro-Artem.local>
2018-02-26 07:35:09 +0300

    finish

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -5,9 +5,10 @@ $( document ).ready(function() {
     nav: false,
     dots: true,
     loop: true,
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     responsive : {
-      1300 : {
-        nav: true
+      1355 : {
+        nav: true,
       }
     }
   });
